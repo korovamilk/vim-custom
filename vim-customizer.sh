9540fe9c8f54505e028371ef413e53823aa805b2
Marco <korovamilk@users.noreply.github.com>
2016-01-27 14:37:30 +0100

    Create vim-customizer.sh

---
+++ vim-customizer.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+# vim-customizer.sh
+# v.20160127
+TODAY=$(date +%Y%m%d)
+VIMHOME=${HOME}/.vim
+echo "This is going to customize vim for user $USER"
+cd ~
+echo 
+echo "safe-copying .vim*"
+mkdir ${VIMHOME}/vim-customizer.${TODAY}.SAFE
+mv .vim* ${VIMHOME}/vim-customizer.${TODAY}.SAFE/
+echo
+echo "creating vim directories"
+mkdir -p ${VIMHOME}/SAFE/vim.backups
+mkdir -p ${VIMHOME}/.vim/colors/
+echo
+echo "getting files"
+curl -LSso ${VIMHOME}/colors/onedark.vim https://raw.githubusercontent.com/korovamilk/onedark.vim/master/colors/onedark.vim
+git clone https://github.com/VundleVim/Vundle.vim.git ${VIMHOME}/bundle/Vundle.vim
+curl -LSso ${VIMHOME}/colors/onedark.vim                    https://raw.githubusercontent.com/korovamilk/onedark.vim/master/colors/onedark.vim
+curl -LSso ${VIMHOME}/colors/summerfruit256.vim             https://raw.githubusercontent.com/korovamilk/summerfruit256.vim/master/colors/summerfruit256.vim
+curl -LSso ${VIMHOME}/colors/tomorrow.vim                   https://raw.githubusercontent.com/korovamilk/vim-tomorrow-theme/master/colors/tomorrow.vim
+curl -LSso ${VIMHOME}/colors/tomorrow-night.vim             https://raw.githubusercontent.com/korovamilk/vim-tomorrow-theme/master/colors/tomorrow-night.vim
+curl -LSso ${VIMHOME}/colors/tomorrow-night-bright.vim      https://raw.githubusercontent.com/korovamilk/vim-tomorrow-theme/master/colors/tomorrow-night-bright.vim
+curl -LSso ${VIMHOME}/colors/tomorrow-night-eighties.vim    https://raw.githubusercontent.com/korovamilk/vim-tomorrow-theme/master/colors/tomorrow-ni-eightiesght.vim
+curl -LSso ${HOME}/.vimrc https://raw.githubusercontent.com/korovamilk/vim-custom/.vimrc
+echo
+echo "installing vim plugins"
+vim +PluginInstall +qall
+echo
+echo "done."
+exit 0
